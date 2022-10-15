2a651c2eac638383cba49b435669827b07c809c6
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2022-10-15 23:21:36 +0200

    update lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,10 +2,26 @@
 const { defineConfig } = require("eslint-define-config")
 
 module.exports = defineConfig({
-  root: true,
-  extends: ["@lehoczky/eslint-config-vue"],
+  plugins: ["simple-import-sort"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+  ],
+  env: {
+    "vue/setup-compiler-macros": true,
+  },
+  parser: "vue-eslint-parser",
   parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ["./tsconfig.eslint.json"],
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  rules: {
+    "no-undef": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/html-self-closing": ["warn", { html: { void: "always" } }],
+    "simple-import-sort/imports": "warn",
   },
 })
