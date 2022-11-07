6e2ec261cdbc6422f8542278cfb9bf146fe6bfb6
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2022-11-07 20:15:58 +0100

    update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,22 @@
-import { defineNuxtConfig } from "nuxt"
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  target: "static",
   ssr: false,
-  head: {
-    link: [
-      { rel: "preconnect", href: "https://fonts.googleapis.com" },
-      {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
-        crossorigin: true,
-      },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap",
-      },
-    ],
+  app: {
+    baseURL: "/frontend-tools/",
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap",
+        },
+      ],
+    },
   },
   css: ["@/assets/css/tailwind.css"],
   modules: ["@vueuse/nuxt"],
