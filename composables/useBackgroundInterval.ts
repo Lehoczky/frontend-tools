438125147206c0b61315202357760db96aa463cb
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2023-02-18 17:03:24 +0100

    use prerendering

--- composables/useBackgroundInterval.ts
+++ composables/useBackgroundInterval.ts
@@ -1,3 +1,30 @@
+export default (callback: () => void) => {
+  let worker: Worker
+
+  onMounted(() => {
+    worker = createWorker(() => {
+      let interval: ReturnType<typeof setInterval> | undefined = undefined
+
+      self.addEventListener("message", (event) => {
+        switch (event.data) {
+          case "start":
+            interval = setInterval(() => self.postMessage("tick"), 1000)
+            break
+          case "pause":
+            clearInterval(interval)
+            break
+        }
+      })
+    })
+
+    worker.addEventListener("message", callback)
+  })
+
+  const startInterval = () => worker?.postMessage("start")
+  const pauseInterval = () => worker?.postMessage("pause")
+  return { startInterval, pauseInterval }
+}
+
 function createWorker(fn: () => void) {
   const blobURL = URL.createObjectURL(
     new Blob(["(", fn.toString(), ")()"], {
@@ -8,27 +35,4 @@ function createWorker(fn: () => void) {
   const worker = new Worker(blobURL)
   URL.revokeObjectURL(blobURL)
   return worker
-}
-
-export default (callback: () => void) => {
-  const worker = createWorker(() => {
-    let interval: ReturnType<typeof setInterval> | undefined = undefined
-
-    self.addEventListener("message", (event) => {
-      switch (event.data) {
-        case "start":
-          interval = setInterval(() => self.postMessage("tick"), 1000)
-          break
-        case "pause":
-          clearInterval(interval)
-          break
-      }
-    })
-  })
-
-  worker.addEventListener("message", callback)
-
-  const startInterval = () => worker.postMessage("start")
-  const pauseInterval = () => worker.postMessage("pause")
-  return { startInterval, pauseInterval }
 }
