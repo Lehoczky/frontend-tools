bbfe92010d8411fdd5d6c55330f1ccab077aeedf
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2023-04-12 17:47:47 +0200

    feat: add new icons

---
+++ components/icon/SVGCrop.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M8 2H6v4H2v2h14v14h2v-4h4v-2h-4V6H8V2zm0 8H6v8h8v-2H8v-6z"
+    />
+  </svg>
+</template>
