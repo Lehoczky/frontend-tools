6b865a6d6677ad95f8e30e60b2295616eca2204a
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2023-05-28 18:10:34 +0200

    style header active link

--- components/Header/HeaderLinkButton.vue
+++ components/Header/HeaderLinkButton.vue
@@ -1,6 +1,12 @@
 <template>
   <NuxtLink
-    class="transition-color inline-flex w-full items-center gap-3 rounded-lg px-4 py-5 ease-out hover:bg-base-400 active:scale-95 sm:rounded-md sm:py-2"
+    class="header-link transition-color inline-flex w-full items-center gap-3 rounded-lg px-4 py-5 ease-out hover:bg-base-400 active:scale-95 sm:rounded-md sm:py-2"
     ><slot
   /></NuxtLink>
 </template>
+
+<style>
+.header-link.router-link-exact-active {
+  @apply bg-base-400;
+}
+</style>
