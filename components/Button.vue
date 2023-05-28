f0f127b6d007c52b73906d3901d43e21696e198f
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2023-05-28 18:10:42 +0200

    add size prop to button

--- components/Button.vue
+++ components/Button.vue
@@ -2,13 +2,15 @@
   <component
     :is="component"
     :to="to"
-    class="transition-color inline-flex items-center gap-1.5 rounded-md px-6 py-4 ease-out active:scale-95 disabled:opacity-50 disabled:active:scale-100 sm:px-4 sm:py-2"
+    class="transition-color inline-flex items-center gap-1.5 rounded-md ease-out active:scale-95 disabled:opacity-50 disabled:active:scale-100"
     :class="{
       'bg-base-500 hover:bg-base-400 disabled:hover:bg-base-500':
         color === 'default',
       'bg-sky-800 hover:bg-sky-700 disabled:hover:bg-sky-800': color === 'blue',
       'bg-rose-800 hover:bg-rose-700 disabled:hover:bg-rose-800':
         color === 'red',
+      'px-6 py-4 sm:px-4 sm:py-2': size === 'default',
+      'px-8 py-5 text-2xl': size === 'large',
     }"
     v-bind="additionalProps"
   >
@@ -22,6 +24,10 @@ import type { RouteLocationRaw } from "vue-router"
 const props = defineProps({
   color: {
     type: String as PropType<"default" | "blue" | "red">,
+    default: "default",
+  },
+  size: {
+    type: String as PropType<"default" | "large">,
     default: "default",
   },
   to: {
--- pages/guides/index.vue
+++ pages/guides/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex flex-col gap-4">
-    <Button to="/guides/development-environment" class="!px-8 !py-5 text-2xl">
+    <Button to="/guides/development-environment" size="large">
       <IconCode class="mr-3 h-10 w-10 shrink-0" />
       <span>Development Environment</span>
     </Button>
 
-    <Button to="/guides/pc-setup" class="!px-8 !py-5 text-2xl">
+    <Button to="/guides/pc-setup" size="large">
       <IconLaptop class="mr-3 h-10 w-10 shrink-0" />
       <span>PC Setup</span>
     </Button>
